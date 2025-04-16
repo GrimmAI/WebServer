7afa51b3843c7cf7411cae79f5b31a090c6e41da
GrimmAI <320208452@qq.com>
2025-04-16 20:46:48 +0800

    add

--- src/test/http_server.cpp
+++ src/test/http_server.cpp
@@ -9,12 +9,6 @@
 #include <sstream>
 #include <json/json.h> // 使用 JSON for Modern C++ 库来处理 JSON 数据
 
-struct ImageResult {
-    std::string description;
-    float score;
-    std::string local_path;
-};
-
 std::string load_html() {
     std::ifstream file("/home/mazhaomeng/cpp/WebServer/src/test/client_api.html");
     if (!file) {
@@ -28,37 +22,6 @@ std::string load_html() {
 
 const std::string html = load_html();
 
-std::string generate_html(const std::vector<ImageResult>& images) {
-    // 构建JSON数组
-    Json::Value jsonData;
-    for (const auto& img : images) {
-        Json::Value item;
-        item["metadata"] = img.description;  // 需要UTF-8编码
-        item["similarity"] = img.score;
-        item["url"] = img.local_path;        // 原始本地路径
-        jsonData.append(item);
-    }
-
-    // 安全转义
-    std::string jsonStr = Json::FastWriter().write(jsonData);
-    std::string safeJson;
-    for (char c : jsonStr) {
-        if (c == '<') safeJson += "\\u003c";
-        else if (c == '>') safeJson += "\\u003e";
-        else if (c == '&') safeJson += "\\u0026";
-        else safeJson += c;
-    }
-
-    // 插入模板
-    std::string html = load_html();
-    size_t pos = html.find("<script id=\"__INITIAL_JSON__\"");
-    if (pos != std::string::npos) {
-        html.replace(pos, 31, "<script id=\"__INITIAL_JSON__\" type=\"application/json\">" + safeJson + "</script>");
-    }
-    return html;
-}
-
-
 void HttpResponseCallback(const HttpRequest &request, HttpResponse *response)
 {
     std::string url = request.url();
@@ -67,6 +30,7 @@ void HttpResponseCallback(const HttpRequest &request, HttpResponse *response)
         response->SetStatusMessage("Bad Request");
         response->SetCloseConnection(true);
     }
+
 
     if (request.method() == HttpRequest::Method::kGet) {
         std::string url = request.url();
@@ -102,48 +66,40 @@ void HttpResponseCallback(const HttpRequest &request, HttpResponse *response)
 
             // 获取用户输入的查询
             std::string query = requestJson["query"].asString();
+            // std::cout << "query: " << query << std::endl;
 
-            // // 模拟图片检索逻辑
-            // std::vector<std::string> image_urls = {
-            //     "/home/mazhaomeng/cpp/WebServer/src/test/images/test.png"
-            // };
 
-            // // 构造 JSON 格式的响应
-            // Json::Value responseJson;
-            // for (const auto& url : image_urls) {
-            //     Json::Value image;
-            //     image["url"] = url;
-            //     image["similarity"] = 0.92; // 模拟相似度
-            //     image["metadata"] = "金毛犬在公园草地奔跑"; // 模拟元数据
-            //     responseJson.append(image);
+            // std::vector<std::string> local_images(100);
+            // for (int i = 0; i < 100; i++) {
+            //     local_images[i] = "https://picsum.photos/300/200?random=" + std::to_string(i);
             // }
+            // 模拟图片检索逻辑
+            std::vector<std::string> local_images = {
+                "https://picsum.photos/300/200?random=1",
+                "https://picsum.photos/300/200?random=2",
+                "https://picsum.photos/300/200?random=3",
+                "https://picsum.photos/300/200?random=4",
+                "https://picsum.photos/300/200?random=5",
+                "http://127.0.0.1:80/test.jpg"
+            };
 
-            // // 将 JSON 响应转换为字符串
-            // Json::StreamWriterBuilder writerBuilder;
-            // std::string jsonResponse = Json::writeString(writerBuilder, responseJson);
+            // 转换本地路径为Web URL
+            Json::Value json_data;
+            for (const auto& local_path : local_images) {
+                Json::Value item;
+                item["url"] = local_path;
+                item["similarity"] = 0.92;
+                item["metadata"] = "金毛犬在公园草地奔跑";
+                json_data.append(item);
+            }
 
-
-            // // 读取HTML模板文件
-            // std::string html_template = load_html();
-
-            // // 在HTML模板中预留一个位置用于插入JSON数据
-            // std::string html_content = html_template;
-            // std::cout << jsonResponse << std::endl;
-            // size_t pos = html_content.find("<!-- JSON_DATA_PLACEHOLDER -->");
-            // if (pos != std::string::npos) {
-            //     html_content.replace(pos, strlen("<!-- JSON_DATA_PLACEHOLDER -->"), "<script>var jsonResponse = " + jsonResponse + ";</script>");
-            //     std::cout << html_content << std::endl;
-            // }
-
-            auto t = new ImageResult();
-            t->local_path = "https://picsum.photos/300/200?random=2";
-            std::vector<ImageResult> a;
-            a.push_back(*t);
-            auto html_content = generate_html(a);
+            // 序列化 JSON 数据
+            Json::StreamWriterBuilder writer;
+            std::string json_str = Json::writeString(writer, json_data);
 
             // 设置响应
             response->SetStatusCode(HttpResponse::HttpStatusCode::k200K);
-            response->SetBody(html_content);
+            response->SetBody(json_str);
             response->SetContentType("application/json");
         } else {
             response->SetStatusCode(HttpResponse::HttpStatusCode::k404NotFound);
